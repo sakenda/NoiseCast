273ad2abf1cfd4db5131c6851249c8b990a7c248
Michael Barth <sakenda1207@gmail.com>
2021-04-08 23:46:41 +0200

    Add project files.

---
+++ WPFMVVM/Core/ObservableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFMVVM.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string property = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+    }
+}
---
+++ WPFMVVM/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using WPFMVVM.Core;
+
+namespace WPFMVVM.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object currentView;
+        public object CurrentView
+        {
+            get { return currentView; }
+            set
+            {
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => CurrentView = HomeVM);
+            DiscoveryViewCommand = new RelayCommand(o => CurrentView = DiscoveryVM);
+        }
+    }
+}
